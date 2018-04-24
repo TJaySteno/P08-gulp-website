82bb92f4d18774836b6e1b007f692a393924bc38
Trent <trent.stenoien@gmail.com>
2018-04-24 14:56:17 -0500

    Rough draft complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,84 @@
-// https://github.com/gulpjs/gulp/tree/v3.9.1/docs
-// https://github.com/robrich/orchestrator
-
-
-
 'use strict'
 
-const gulp = require('gulp');
+// Require dependencies
+const gulp     = require('gulp');
+const del      = require('del');
+const cleanCSS = require('gulp-clean-css');
+const concat   = require('gulp-concat');
+const connect  = require('gulp-connect');
+const imagemin = require('gulp-imagemin');
+const rename   = require("gulp-rename");
+const replace  = require('gulp-replace');
+const sass     = require('gulp-sass');
+const maps     = require('gulp-sourcemaps');
+const uglify   = require('gulp-uglify');
 
-// gulp scripts command:
-  // concatenate, minify, copy all js files into all.min.js file
-    // Then copied to the dist/scripts folder
-  // Generate source maps
+// Locations of src and dist directories
+const options = {
+  src: './src',
+  dist: './dist'
+}
+
+// 'gulp scripts': concat & minify all JS files, copy to ./dist/scripts/all.min.js
 gulp.task('scripts', function () {
-  console.log('scripts');
-});
+  gulp.src(options.src + '/js/**/*.js')
+    .pipe(maps.init())
+      .pipe(concat('global.js'))
+      .pipe(uglify())
+    .pipe(maps.write())
+    .pipe(rename("all.min.js"))
+    .pipe(gulp.dest(options.dist + '/scripts')) });
 
-// gulp styles command
-  // compile SCSS files into CSS
-    // Then concatenate and minify into all.min.css file
-      // Then copied to the dist/styles folder
-  // source maps are generated
+// 'gulp styles': compile SCSS into CSS, concat & minify, then copy to ./dist/styles/all.min.css
 gulp.task('styles', function () {
-  console.log('styles');
-});
+  return gulp.src(options.src + '/sass/**/*.scss')
+    .pipe(maps.init())
+      .pipe(sass())
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(maps.write('../maps'))
+    .pipe(rename("all.min.css"))
+    .pipe(gulp.dest(options.dist + '/styles')) });
 
-// gulp images command:
-  // optimize size of JPEG and PNG files
-    // Then copy optimized images to the dist/content folder
+// 'gulp images': optimize images and copy into ./dist/content
 gulp.task('images', function () {
-  console.log('images');
-});
+  gulp.src(options.src + '/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest(options.dist + '/content')) });
 
-// gulp clean command
-  // delete all of the files and folders in the dist folder
+// 'gulp clean': delete all files & folders in ./dist
 gulp.task('clean', function () {
-  return console.log('clean');
-});
+  return del(options.dist + '/**') });
 
-// gulp build command
-  // run the clean, scripts, styles, and images tasks
-    // clean task completes before the rest
+// 'gulp build': optimize scripts, styles, & images for distribution
 gulp.task('build', ['clean'], function () {
-  console.log('build');
-  return gulp.start('scripts', 'styles', 'images');
-});
+  return gulp.start(['html', 'scripts', 'styles', 'images']) });
 
-// gulp command:
-  // run the build task and serve my project using a local web server.
-  // continuously watch for changes to any .scss file
-    // on change, run gulp styles command
-      // reload project in the browser
+// 'gulp connect': start a local web server on port 3000
+gulp.task('connect', function() {
+  connect.server({
+    root: './dist',
+    livereload: true,
+    port: 3000 }) });
+
+// 'gulp html': copy index.html to ./dist
+gulp.task('html', function () {
+  gulp.src(options.src + '/index.html')
+    .pipe(replace('css/', 'styles/'))
+    .pipe(replace('js/', 'scripts/'))
+    .pipe(replace('images/', 'content/'))
+    .pipe(replace('/global.', '/all.min.'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload()) });
+
+// 'gulp loadNewStyles': call 'gulp styles'; once complete, refresh the browser
+gulp.task('loadNewStyles', ['styles'], function () {
+  gulp.src(options.dist + '/index.html')
+    .pipe(connect.reload()) });
+
+// 'gulp watch': on changes to Sass files, recompile and refresh page
+gulp.task('watch', function () {
+  gulp.watch([options.src + '/sass/**/*.scss'], ['loadNewStyles']) });
+
+// 'gulp': build the page, then serve project on a local web server and watch for changes to Sass files
 gulp.task('default', ['build'], function () {
-  console.log('default');
-});
+  gulp.start(['connect', 'watch']) });
